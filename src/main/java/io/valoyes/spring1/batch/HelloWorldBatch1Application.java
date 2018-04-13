1553c72d891e6230a951b413ce9d3879c776529f
bavaloyes <bavaloyes@ITEM-S35533.emea.msad.sopra>
2018-04-13 22:15:08 +0200

    init commit

---
+++ src/main/java/io/valoyes/spring1/batch/HelloWorldBatch1Application.java
@@ -0,0 +1,15 @@
+package io.valoyes.spring1.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class HelloWorldBatch1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldBatch1Application.class, args);
+	}
+}
---
+++ src/test/java/io/valoyes/spring1/batch/HelloWorldBatch1ApplicationTests.java
@@ -0,0 +1,16 @@
+package io.valoyes.spring1.batch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloWorldBatch1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
